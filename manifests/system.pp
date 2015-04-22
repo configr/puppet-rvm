f4da4f9133dbb3005579407e39447d888325297e
Felipe Arenhardt Tomaz <felipe.a.tomaz@gmail.com>
2015-04-22 14:26:41 -0300

    Change rvm-installer args

--- manifests/system.pp
+++ manifests/system.pp
@@ -18,7 +18,7 @@ class rvm::system($version=undef) {
     path    => '/usr/bin:/usr/sbin:/bin',
     command => "bash -c '/usr/bin/curl -sL https://get.rvm.io -o /tmp/rvm-installer && \
                 chmod +x /tmp/rvm-installer && \
-                rvm_bin_path=/usr/local/rvm/bin rvm_man_path=/usr/local/rvm/man /tmp/rvm-installer --version ${actual_version} && \
+                rvm_bin_path=/usr/local/rvm/bin rvm_man_path=/usr/local/rvm/man /tmp/rvm-installer -s ${actual_version} && \
                 rm /tmp/rvm-installer'",
     creates => '/usr/local/rvm/bin/rvm',
     require => [
