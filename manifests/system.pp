3e5b0f9a29091f2cf96cc20a36db95aa5d6e82be
Felipe Tomaz <felipe@felipetomaz.com>
2015-04-22 17:27:52 -0300

    Change rvm-gpg-add to execute only on refresh

--- manifests/system.pp
+++ manifests/system.pp
@@ -9,6 +9,7 @@ class rvm::system($version=undef) {
   exec { 'rvm-gpg-add':
     path    => '/usr/bin:/usr/sbin:/bin',
     command => 'gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3',
+    refreshonly => true,
     require => [
       Class['rvm::dependencies'],
     ],
