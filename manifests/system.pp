4c238b279205336c7894f90817c392857e442225
Felipe Tomaz <felipe@felipetomaz.com>
2015-04-22 17:54:17 -0300

    Chang args and refresh

--- manifests/system.pp
+++ manifests/system.pp
@@ -5,11 +5,10 @@ class rvm::system($version=undef) {
     'present' => 'latest',
     default   => $version,
   }
-  
+
   exec { 'rvm-gpg-add':
     path    => '/usr/bin:/usr/sbin:/bin',
     command => 'gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3',
-    refreshonly => true,
     require => [
       Class['rvm::dependencies'],
     ],
@@ -19,7 +18,7 @@ class rvm::system($version=undef) {
     path    => '/usr/bin:/usr/sbin:/bin',
     command => "bash -c '/usr/bin/curl -sL https://get.rvm.io -o /tmp/rvm-installer && \
                 chmod +x /tmp/rvm-installer && \
-                rvm_bin_path=/usr/local/rvm/bin rvm_man_path=/usr/local/rvm/man /tmp/rvm-installer -s ${actual_version} && \
+                rvm_bin_path=/usr/local/rvm/bin rvm_man_path=/usr/local/rvm/man /tmp/rvm-installer --version ${actual_version} && \
                 rm /tmp/rvm-installer'",
     creates => '/usr/local/rvm/bin/rvm',
     require => [
