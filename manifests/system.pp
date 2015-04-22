91ecd6f9557160cf519a58c952188061f260e614
Felipe Arenhardt Tomaz <felipe.a.tomaz@gmail.com>
2015-04-22 12:41:02 -0300

    Update system.pp
    Add gpg key to install rvm

--- manifests/system.pp
+++ manifests/system.pp
@@ -5,16 +5,25 @@ class rvm::system($version=undef) {
     'present' => 'latest',
     default   => $version,
   }
+  
+  exec { 'rvm-gpg-add':
+    path    => '/usr/bin:/usr/sbin:/bin',
+    command => 'gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3',
+    require => [
+      Class['rvm::dependencies'],
+    ],
+  }
 
   exec { 'system-rvm':
     path    => '/usr/bin:/usr/sbin:/bin',
-    command => "bash -c '/usr/bin/curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer -o /tmp/rvm-installer && \
+    command => "bash -c '/usr/bin/curl -sL https://get.rvm.io -o /tmp/rvm-installer && \
                 chmod +x /tmp/rvm-installer && \
                 rvm_bin_path=/usr/local/rvm/bin rvm_man_path=/usr/local/rvm/man /tmp/rvm-installer --version ${actual_version} && \
                 rm /tmp/rvm-installer'",
     creates => '/usr/local/rvm/bin/rvm',
     require => [
       Class['rvm::dependencies'],
+      Exec['rvm-gpg-add'],
     ],
   }
 
